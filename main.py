e91ad74b5debcf49b430e042c183d07621a5c8e0
Gianlz <gianlucazugnoifc@gmail.com>
2025-04-01 22:04:14 -0300

    feat: created retrieve data in main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from app.controllers.todo import add_to_DB
+from app.controllers.todo import add_to_DB, get_all
+from app.models.todo import TodoTable
+
 
 # Entry Point of the code'
+
+# Teste de adição
 add_to_DB("tarefa1", "Tarefa muito foda feita por mim, sem validação")
+
+# Teste de recuperar dados
+content = get_all()
+for todo in content:
+    print(f"ID:{todo._id}\nName [{todo.name}]\nDescription [{todo.description}]\nCompleted [{todo.completed}]")
